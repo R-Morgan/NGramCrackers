58378e972a39b06e79448bdd259733a330d42a28
R-Morgan <rmorgan@trystero.is>
2014-11-22 21:52:44 -0800

    Add TupleManipulation functions

---
+++ src/NGramCrackers/TupleManipulation.hs
@@ -0,0 +1,36 @@
+module SolvingEuler.TupleManipulation
+(
+thrd,
+compareDoubles,
+compareDoublesInList, 
+compareTriples,
+compareTriplesInList
+) where
+
+thrd :: (a , b , c) -> c
+thrd (_, _, x) = x
+
+compareDoubles:: Ord b => (a, b) -> (a, b) -> (a, b)
+compareDoubles xs  ys
+    | max (snd xs) (snd ys) == (snd xs) = xs
+    | max (snd xs) (snd ys) == (snd ys) = ys
+    | otherwise = error "Fail"
+
+compareDoublesInList :: Ord b => [(a, b)] -> (a, b)
+compareDoublesInList []     = error "Empty List"
+compareDoublesInList [x]    = x 
+compareDoublesInList (x:xs) = compareDoubles x (compareDoublesInList xs) 
+
+
+compareTriples :: Ord c => (a, b, c) -> (a, b, c) -> (a, b, c)
+compareTriples xs ys
+    | max (thrd xs) (thrd ys) == (thrd xs) = xs
+    | max (thrd xs) (thrd ys) == (thrd ys) = ys
+    | otherwise = error "Fail"
+
+
+compareTriplesInList :: Ord c => [(a, b, c)] -> (a, b, c)
+compareTriplesInList []     = error "Empty List"
+compareTriplesInList [x]    = x 
+compareTriplesInList (x:xs) = compareTriples x (compareTriplesInList xs) 
+
