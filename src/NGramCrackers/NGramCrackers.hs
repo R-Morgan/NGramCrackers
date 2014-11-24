26668243b5b77cf15eaf107e651ecc0b2bbdbbf5
R-Morgan <rmorgan@trystero.is>
2014-11-24 14:58:14 -0800

    Add haddock-style comments to functions

--- src/NGramCrackers/NGramCrackers.hs
+++ src/NGramCrackers/NGramCrackers.hs
@@ -10,16 +10,22 @@ module NGramCrackers.NGramCrackers (
 import Data.Char (isAlpha, toLower)
 import ListManipulation
 
+{-| Extract bigrams from a string -}
 bigrams :: [Char] -> [String]
 bigrams wordString = getNGramsFromString 2 wordString
 
+{-| Extract trigrams from a string -}
 trigrams :: [Char] -> [String]
 trigrams wordString = getNGramsFromString 3 wordString
 
+
+{-| Extract n-grams from a string -}
 getNGramsFromString :: Int -> [Char] -> [String]
 getNGramsFromString n wordString = map unwords $ getNGramsFromList n wordList
     where wordList = getAlphasOnlyToList wordString
 
+{-| Extract n-grams from a List. Internal function for n-gram
+    string extraction function -}
 getNGramsFromList :: Int -> [String] -> [[String]]
 getNGramsFromList n wordList = getNSeqFromList n wordList 
 
