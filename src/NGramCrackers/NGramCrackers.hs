2f8fbb24a65e2387707eb2813a13859e3be67786
R-Morgan <rmorgan@trystero.is>
2014-11-24 20:35:39 -0800

    Add getWordFrequency function

--- src/NGramCrackers/NGramCrackers.hs
+++ src/NGramCrackers/NGramCrackers.hs
@@ -8,35 +8,36 @@ module NGramCrackers.NGramCrackers (
 ) where
 
 import Data.Char (isAlpha, toLower)
-import ListManipulation
+import NGramCrackers.ListManipulation
 
 {-| Extract bigrams from a string -}
-bigrams :: [Char] -> [String]
-bigrams wordString = getNGramsFromString 2 wordString
+bigrams :: String -> [String]
+bigrams = getNGramsFromString 2 
 
 {-| Extract trigrams from a string -}
-trigrams :: [Char] -> [String]
-trigrams wordString = getNGramsFromString 3 wordString
-
+trigrams :: String -> [String]
+trigrams = getNGramsFromString 3 
 
 {-| Extract n-grams from a string -}
-getNGramsFromString :: Int -> [Char] -> [String]
+getNGramsFromString :: Int -> String -> [String]
 getNGramsFromString n wordString = map unwords $ getNGramsFromList n wordList
     where wordList = getAlphasOnlyToList wordString
 
 {-| Extract n-grams from a List. Internal function for n-gram
     string extraction function -}
 getNGramsFromList :: Int -> [String] -> [[String]]
-getNGramsFromList n wordList = getNSeqFromList n wordList 
+getNGramsFromList = getNSeqFromList 
 
 {-| Return only alphabetic characters from a string and return the
     result as a string. Output of this function may need processing
     into a list, tuple, etc. -}
-getAlphasOnlyToString :: [Char] -> [Char]
-getAlphasOnlyToString = (unwords . map (filter (isAlpha)) . words)
+getAlphasOnlyToString :: String -> String
+getAlphasOnlyToString = unwords . map (filter isAlpha) . words
 
 {-| Return only alphabetic characters from a string and return the
     result as a List.-}
-getAlphasOnlyToList :: [Char] -> [String]
-getAlphasOnlyToList = (map (filter (isAlpha)) . words . (map toLower))
+getAlphasOnlyToList :: String -> [String]
+getAlphasOnlyToList = map (filter isAlpha) . words . map toLower
 
+getNWordFrequency:: String -> String -> Int
+getNWordFrequency word text = (length . filter (== word) . words) text
