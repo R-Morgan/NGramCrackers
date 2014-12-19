33fcc598d774820ca9674b040eac91fac250de8a
R-Morgan <rmorgan@trystero.is>
2014-12-19 14:43:15 -0800

    Attempt basic Read/Write functionality
      -- The mainLoop function is a roundabout way of testing
         line-by-line read/write operations. Borrowed from RWH.
         This is obviously not the final functionality. Going
         to modify it to parse the string.

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,20 @@
 import System.IO
-import NGramCrackers.NGramCrackers
+--import NGramCrackers.NGramCrackers
+import Data.Char (toUpper)
 
 main = do
-    handle   <- openFile "NGramCrackers/story.txt" ReadMode
-    contents <- hGetContents handle 
-    print $ bigrams contents
-    hClose handle
+    inHandle   <- openFile "NGramCrackers/story.txt" ReadMode
+    outHandle  <- openFile "NGramCrackers/processedStory.txt" WriteMode
+--    contents <- hGetContents inHandle 
+    mainLoop inHandle outHandle
+    --hPutStrLn outHandle print $ bigrams contents
+    hClose inHandle
+    hClose outHandle
+
+mainLoop :: Handle -> Handle -> IO ()
+mainLoop inHandle outHandle = do isEnd <- hIsEOF inHandle
+                                 if isEnd
+                                   then return ()
+                                   else do inputStr <- hGetLine inHandle
+                                           hPutStrLn outHandle (map toUpper inputStr)
+                                           mainLoop inHandle outHandle
