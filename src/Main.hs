a60ca05ef77009125d5cae990e3d19f74795c391
R-Morgan <rmorgan@trystero.is>
2014-12-28 16:49:45 -0800

    Clean up scratch code

--- src/Main.hs
+++ src/Main.hs
@@ -16,21 +16,6 @@ main = do
          Left e  -> do putStrLn "Error parsing input: "
                        print e
 
-         --Right r -> mapM_ putStrLn $ map stringifyLexemeCount $ (lexemeCountProfile . words . concat) r
-         Right r   -> mapM_ putStrLn $ map stringifyLexemeCount $ lexemeCountProfile $ concat r
-    --eof
-
-    --hPutStrLn outHandle $ (concat . intersperse "\n" . bigrams) contents
-    -- putStrLn $ fromRight $ liftM concat $ liftM (intersperse "\n") $ liftM unwords $ liftM concat $ parseParagraph contents
---    putStrLn $ fromRight $ liftM unwords $ liftM (intersperse "\n") $ liftM concat $ parseParagraph contents
+         Right r   -> mapM_ (putStrLn . stringifyLexemeCount) (lexemeCountProfile $ concat r)
     hClose inHandle
     hClose outHandle
-
-{- mainLoop :: Handle -> Handle -> IO ()
-mainLoop inHandle outHandle = do isEnd <- hIsEOF inHandle
-                                 if isEnd
-                                   then return ()
-                                   else do inputStr <- hGetLine inHandle
-                                           hPutStrLn outHandle (map toUpper inputStr)
-                                           mainLoop inHandle outHandle
--}
