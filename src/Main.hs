d16651c6186196ba8b51d8c713f279c32dcb8db2
R-Morgan <rmorgan@trystero.is>
2014-12-30 19:14:08 -0800

    Add CLI arg parsing skeleton
       -- The arg parsing library used is GetOpts. The program compiles
          and outputs that it has zero arguments. The code was borrowed
          from http://leiffrenzel.de/papers/commandline-options-in-haskell.html

--- src/Main.hs
+++ src/Main.hs
@@ -1,17 +1,22 @@
 import System.IO
-import Text.ParserCombinators.Parsec (eof)
+import System.Environment (getArgs, getProgName)
 import NGramCrackers.NGramCrackers
 import NGramCrackers.TupleManipulation
 import NGramCrackers.ParagraphParsers
 import Data.Char (toUpper)
 import Data.List (intersperse)
 import Control.Monad (liftM)
-import Data.Either.Unwrap (fromRight)
+import System.Console.GetOpt
+
+-- removed some extraneous imports 
+
 
 main = do
-    inHandle   <- openFile "NGramCrackers/story.txt" ReadMode
-    outHandle  <- openFile "NGramCrackers/processed.csv" WriteMode
-    contents   <- hGetContents inHandle -- contents :: String
+    argv       <- getArgs 
+    progName       <- getProgName
+    inHandle       <- openFile "NGramCrackers/story.txt" ReadMode
+    outHandle      <- openFile "NGramCrackers/processed.csv" WriteMode
+    contents       <- hGetContents inHandle -- contents :: String
     
     case parseParagraph contents of 
          Left e  -> do putStrLn "Error parsing input: "
@@ -20,5 +25,17 @@ main = do
          Right r -> hPutStrLn outHandle "word,count" >> 
                     mapM_ (hPutStrLn outHandle . doubleToCSV) (lexemeCountProfile $ concat r)
 
+    let ( flags, nonOpts, msgs ) = getOpt RequireOrder options argv
+    print $ length flags
+    
     hClose inHandle
     hClose outHandle
+
+options :: [OptDescr a]
+options = []
+
+{- dispatch :: [(String, String -> IO ())]
+dispatch = [ ("inf", inf)
+           , ("outf", outf)
+           ]
+-}
