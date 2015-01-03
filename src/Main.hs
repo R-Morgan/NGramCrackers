8f6578b823ba9580bca922490e369c91a7c205bc
R-Morgan <rmorgan@trystero.is>
2015-01-02 21:15:37 -0800

    Add basic CLI arg parsing facilities, using CmdArgs
      -- I have abandoned using GetOpts. It's too complicated and
         low-level. CmdArgs allows for easy specification of args
         and help strings.
    
      -- Args is a data type to represent the various options that
         the prog can take.
    
      -- myArgs is a record (struct-likething?) of the arguments so
         far.
    
      -- The optionHandler function seems to be a case of pattern
         matchin to define the behaviours of each. However, the
         function does not seem to account for when multiple
         args are passed to the prog.
    
      -- The Type signatures are all screwy right now. The example code
         that inspired this did not have much in terms of type sigs.
    
      -- The implementation outputs to stdout, but it would be useful to
         to allow for output to a file. This may allow for somewhat less
         stringent helper functions (e.g., countWords, typeTokenRatio).

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+
+import System.Exit 
 import System.IO
 import System.Environment (getArgs, getProgName)
 import NGramCrackers.NGramCrackers
@@ -6,45 +9,49 @@ import NGramCrackers.ParagraphParsers
 import Data.Char (toUpper)
 import Data.List (intersperse)
 import Control.Monad (liftM)
-import System.Console.GetOpt
+import System.Console.CmdArgs
+import Data.Maybe (fromMaybe)
+import Data.List (genericLength, nub)
 
--- removed some extraneous imports 
+data Args = Args {wordC :: Bool, ttr :: Bool} deriving (Show, Data, Typeable)
 
-data Flag = Version
+myArgs = Args {  wordC = def &= name "wc" &= help "Print word count"
+               , ttr   = def &= help "Print type token ration (ttr)" }
 
-main = do
+--optionHandler :: Args
+optionHandler Args { wordC = True } = countWords
+optionHandler Args { ttr   = True } = typeTokenRatio
+optionHandler _ = fullMonty
+
+countWords = show . length . words
+
+typeTokenRatio ::  String -> String
+typeTokenRatio string = "Types: " ++ show types ++ ", Tokens: " ++ show tokens ++ ", TTR: "++ show ratio  
+                       where types = (genericLength . nub . words) string
+                             tokens = (genericLength . words) string
+                             ratio = types / tokens
+
+fullMonty :: String -> String
+fullMonty tokens = "Count: " ++ count ++ ", " ++ ttr where
+                    count = (show . length . words) tokens
+                    ttr   = typeTokenRatio tokens
+
+main = cmdArgs myArgs >>= interact . optionHandler
+
+{- do
     argv       <- getArgs 
     progName       <- getProgName
     inHandle       <- openFile "NGramCrackers/story.txt" ReadMode
     outHandle      <- openFile "NGramCrackers/processed.csv" WriteMode
     contents       <- hGetContents inHandle -- contents :: String
     
-    case parseParagraph contents of 
+   case parseParagraph input of 
          Left e  -> do putStrLn "Error parsing input: "
                        print e
 
          Right r -> hPutStrLn outHandle "word,count" >> 
                     mapM_ (hPutStrLn outHandle . doubleToCSV) (lexemeCountProfile $ concat r)
 
-    --let ( flags, nonOpts, msgs ) = getOpt RequireOrder options argv
-    --print $ length flags
-
-    case getOpt RequireOrder options argv of
-      (flags, [], []) -> print $ length flags
-      ([], nonOpts, []) -> error $ "unrecognised arguments: " ++ unwords nonOpts
-      (_, _, errs)      -> error $ concat errs ++ usageInfo header options
-
     hClose inHandle
     hClose outHandle
-
-options :: [OptDescr Flag]
-options = [Option ['v'] ["version"] (NoArg Version) "show version number"]
-
-header :: String
-header = "Usage: main [OPTION..]"
-
-{- dispatch :: [(String, String -> IO ())]
-dispatch = [ ("inf", inf)
-           , ("outf", outf)
-           ]
 -}
