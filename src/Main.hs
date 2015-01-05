6b4a11685ba7ed0ab132064223dc90fae7a731d6
R-Morgan <rmorgan@trystero.is>
2015-01-04 22:00:23 -0800

    Refactor and edit code
      -- Remove extraneous import statements left over from early
         development
    
      -- Remove old summary and programme name annotations from
         myArgs record. These were replaced with public domain
         code authored by Shinobu. These options are found in
         getOpts. These descriptive annotations are also further
         abstracted from by defining a function to output the
         descriptive string.
    
      -- Add exec, optionHandler, and getOpt to modularise the
         command argument parsing. storing the intended output strings
    
      -- Modify main function to use getOpt and optionHandler
         and desugared syntax.
    
         The desugared function binds the output of getArgs
         to the variable in the lambda 'arg'. The result of
         the parenthetical function is then bound to the
         optionHandler function. Monads are sweet.
    
      -- Delete old main function, because CmdArgs handles the
         the fetching of input and writing to output. Removed
         case expression and put into an as yet untracked file
         in preparation for v0.1.0 release.
    
      -- Delete notices for lack of -w or -t flags. Since they
         are optional and did not cause the programme to fail, I
         felt it unnecessary to include them.- Delete old main function.

--- src/Main.hs
+++ src/Main.hs
@@ -2,15 +2,12 @@
 
 import System.Exit 
 import System.IO
-import System.Environment (getArgs, getProgName)
+import System.Environment (getArgs, getProgName, withArgs)
 import NGramCrackers.NGramCrackers
 import NGramCrackers.TupleManipulation
 import NGramCrackers.ParagraphParsers
-import Data.Char (toUpper)
-import Data.List (intersperse)
 import Control.Monad (liftM, when)
 import System.Console.CmdArgs
-import Data.Maybe (fromMaybe)
 import Data.List (genericLength, nub)
 
 data Args = Args {  wordC  :: Bool
@@ -25,19 +22,41 @@ myArgs = Args {  wordC = def &= name "wc" &= help "Print word count"
                , input = def &= typFile &= help "Input file"
                , output = def &= typFile &= help "Output file"
                }
-               &= summary ("NGramCrackers CLI (C) R. Morgan.  The " ++
-                           "source code provided here is licenced under " ++
-                           "the GPLv 3 or greater. The binary is a command " ++
-                           "line tool for quantitative text analysis.")
-               &= program "NGramCrackers CLI v0.0.1"
+
+main :: IO ()
+main = getArgs >>= \args ->  
+       (if null args then withArgs ["--help"] else id) getOpts >>= optionHandler
 
 optionHandler :: Args -> IO ()
 optionHandler opts@Args{..} = do
-     when (not wordC) $ putStrLn "For word count, add -wc"
-     when (not ttr)   $ putStrLn "For TTR, add -t"
      when (null input) $ putStrLn "Supply input file" >> exitWith (ExitFailure 1)
      when (null output) $ putStrLn "Supply output file" >> exitWith (ExitFailure 1)
      exec opts
+
+
+getOpts :: IO Args
+getOpts = cmdArgs $ myArgs
+    &= verbosityArgs [explicit, name "Verbose", name "V"] []
+    &= versionArg [explicit, name "version", name "v", summary _PROGRAM_INFO]
+    &= summary (_PROGRAM_INFO ++ ", " ++ _COPYRIGHT)
+    &= help _PROGRAM_ABOUT
+    &= helpArg [explicit, name "help", name "h"]
+    &= program _PROGRAM_NAME
+
+_PROGRAM_NAME :: String
+_PROGRAM_NAME = "NGramCrackers CLI"
+
+_PROGRAM_VERSION :: String
+_PROGRAM_VERSION = "0.1.0"
+
+_PROGRAM_INFO :: String
+_PROGRAM_INFO = _PROGRAM_NAME ++ " version " ++ _PROGRAM_VERSION
+
+_PROGRAM_ABOUT :: String
+_PROGRAM_ABOUT = "A CLI utility for quantitative text analysis"
+
+_COPYRIGHT :: String
+_COPYRIGHT = "(C) Rianna Morgan 2015"
 
 exec :: Args -> IO ()
 exec opts@Args{..} = do inHandle <- openFile (input) ReadMode 
@@ -48,12 +67,7 @@ exec opts@Args{..} = do inHandle <- openFile (input) ReadMode
                         hClose inHandle
                         hClose outHandle
 
-
-{- optionHandler Args { wordC = True } = countWords
-optionHandler Args { ttr   = True } = typeTokenRatio
-optionHandler _ = fullMonty
--}
-
+countWords :: String -> String
 countWords = show . length . words
 
 typeTokenRatio ::  String -> String
@@ -67,15 +81,8 @@ fullMonty tokens = "Count: " ++ count ++ ", " ++ ttr where
                     count = (show . length . words) tokens
                     ttr   = typeTokenRatio tokens
 
-main = cmdArgs myArgs >>= optionHandler
 
-{- do
-    argv       <- getArgs 
-    progName       <- getProgName
-    inHandle       <- openFile "NGramCrackers/story.txt" ReadMode
-    outHandle      <- openFile "NGramCrackers/processed.csv" WriteMode
-    contents       <- hGetContents inHandle -- contents :: String
-    
+{- 
    case parseParagraph input of 
          Left e  -> do putStrLn "Error parsing input: "
                        print e
@@ -83,6 +90,4 @@ main = cmdArgs myArgs >>= optionHandler
          Right r -> hPutStrLn outHandle "word,count" >> 
                     mapM_ (hPutStrLn outHandle . doubleToCSV) (lexemeCountProfile $ concat r)
 
-    hClose inHandle
-    hClose outHandle
 -}
