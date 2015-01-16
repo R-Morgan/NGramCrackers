efce39cd481f50b6820f68f307b46890538eb86b
R-Morgan <rmorgan@trystero.is>
2015-01-15 20:29:58 -0800

    Add test for getNSeqFromList
      -- getNSeqProp is extremely inefficient, because it generates many
         invalid test cases that fail.  This will likely need to be
         addressed by creating custom generators.

--- testsuite/ngcTests.hs
+++ testsuite/ngcTests.hs
@@ -1,15 +1,17 @@
 import Test.QuickCheck
-import NGramCrackers.NGramCrackers
+import NGramCrackers.ListManipulation
 
-{-| Test whether all bigrams are 2 words long -}
+{-| Test whether all bigrams are 2 words long 
 quickCheck (\s -> all ( == 2) (map length $ bigrams s))
 
 
-{-| Test that getNSeqFromList 2 returns lists of length 2 -}
 quickCheck (\s -> all (==2) (map length $ getNSeqFromList 2 s)) 
 
 quickCheck (\n s -> all (==n) (map length $ getNSeqFromList n s)) 
 
 quickCheck (\n s -> all (==n) (map length $ getNSeqFromString n s)) 
 
+| Test that getNSeqFromList 2 returns lists of length 2 -}
 
+getNSeqProp n s = n <= length s && n > 0 && n < 7 ==> all (== n) (map length $ getNSeqFromList n s)
+  where types = s::[String]
