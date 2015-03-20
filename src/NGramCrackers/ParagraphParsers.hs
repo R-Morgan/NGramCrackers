3503ee828a680a01de58400647d4ef58e6a40584
R-Morgan <rmorgan@trystero.is>
2015-03-19 19:43:58 -0700

    Clean up ParagraphParsers, add new parsing functions
     - Delete hanging white space at ends of lines
    
     - Add several Control.Applicative functions to aid in parsing
       The infix operators help with choice in parsing streams.
       liftA3 is needed to parse DateS and other sorts of functions
       that will need multiple arguments.
    
     - Clean up seppr
    
     - Restructure eop to use monadic and applicative parsing. Since
       what is returned doesn't really matter so much, the value is
       discarded.
    
     - tagParser and contentParser are functions that are useful
       for mtParser and returning a Parser MetaTag. metadataParser
       returns a Parser [MetaTag]. The MetaTag parsing could run
       through the different tags and store all of them and their
       fields easily. However, this may not be the best way to
       construct a safe parser.
    
     - dateParser parses a Text into a Date type.
    
     - sDateParser does the same thing for SDateS.

--- src/NGramCrackers/ParagraphParsers.hs
+++ src/NGramCrackers/ParagraphParsers.hs
@@ -7,48 +7,48 @@ module NGramCrackers.ParagraphParsers (
 ) where
 
 
-import Control.Applicative ((<$>), (*>), (<|>))                                 
+import Control.Applicative ((<$>), (<*), (*>), (<*>), (<|>), liftA3)
 import Data.Functor (void)
 import Data.List (concat, unwords)
-import Data.Text as T                                                           
-import Text.Parsec.Text as PT                                                   
-import Text.ParserCombinators.Parsec hiding ((<|>)) 
+import Data.Text as T                    
+import Text.Parsec.Text as PT 
+import Text.ParserCombinators.Parsec hiding ((<|>))
+import NGramCrackers.DataTypes
 
-parseSent :: T.Text -> Either ParseError [T.Text]                               
-parseSent = parse sentence "unknown" 
+parseSent :: T.Text -> Either ParseError [T.Text]
+parseSent = parse sentence "unknown"
 
 parseParagraph :: T.Text -> Either ParseError [[T.Text]]
-parseParagraph = parse paragraph "unknown" 
+parseParagraph = parse paragraph "unknown"
 
---parseMultiPara :: T.Text ->  Either ParseError [[[T.Text]]]
---parseMultiPara = parse document "unknown" 
+parseMultiPara :: T.Text ->  Either ParseError [[[T.Text]]]
+parseMultiPara = parse document "unknown"
 
---document :: PT.Parser [[[T.Text]]]
---document = endBy paragraph eop
+document :: PT.Parser [[[T.Text]]]
+document = endBy paragraph eop
 
 paragraph :: PT.Parser [[T.Text]]
 paragraph = endBy sentence eos
 
 sentence :: PT.Parser [T.Text]
-sentence = sepBy word seppr 
+sentence = sepBy word seppr
 
 word :: PT.Parser T.Text
 -- The use of T.pack <$> is necessary because of the type many1 letter returns.
 -- fmapping T.pack into the Parser makes it possible to return a parser of the
 -- appropriate type.
-word = T.pack <$> many1 letter
-                                                                                   
+word = T.pack <$> ((many1 letter) <|> (many1 digit))
+                                                     
 seppr :: PT.Parser ()
 -- Since the results of this parser are just thrown away, we need the `void`
 -- function from Data.Functor
-seppr =  void sepprs <|> void newLn                                        
-           where sepprs =    space'                                                 
-                         <|> (char ',' *> space')                                   
-                         <|> (char ';' *> space')                                   
-                         <|> (char ':' *> space')                                   
-                 newLn  =    ((many1 (char '\n')))                         
+seppr =  void sepprs <|> void newLn
+           where sepprs =    space'
+                         <|> (char ',' *> space')
+                         <|> (char ';' *> space')
+                         <|> (char ':' *> space')
+                 newLn  =    ((many1 (char '\n')))
                  space' = char ' '
-
 
 eos :: PT.Parser ()
 eos = void sepprs -- <|> void sngls
@@ -61,8 +61,48 @@ eos = void sepprs -- <|> void sngls
 -}
               space' =    char ' '
 
---eop :: PT.Parser ()
---eop = void "<para>"
+eop :: PT.Parser ()
+eop = void $ 
+  char '<' >> many1 letter >> char '>' *> (void space' <|> void newLn)
+    where space' = char ' '
+          newLn  = (many1 (char '\n'))
+
+{- Metadata parsing function -}
+
+tagParser :: PT.Parser T.Text
+tagParser = (between left right $ parser) <* many space'
+              where left   = (char '<')
+                    right  = (char '>') 
+                    parser = pack <$> (many1 letter)
+                    space' = char ' '
+
+contentsParser :: PT.Parser T.Text
+contentsParser = pack <$> ((letts  <|> nums) <* (many space'))
+                   where letts  = (many1 letter)
+                         nums   = (many1 digit)
+                         space' = char ' '
+
+mtParser :: PT.Parser MetaTag
+mtParser = toMT <$> tagParser <*> contentsParser
+
+metadataParser :: PT.Parser [MetaTag]
+metadataParser = sepBy mtParser newLn 
+                   where newLn = (char '\n')
+
+dateParser :: PT.Parser Date
+dateParser = liftA3 toDate month day year
+               where month = dP toMonth <$> (many1 digit) <* seppr
+                     day   = dP toDay   <$> (many1 digit) <* seppr
+                     year  = dP toYear  <$> (many1 digit)
+                     seppr = (char '/')
+                     dP f  = f . read
+
+sDateParser :: PT.Parser SDate
+sDateParser = toSDate <$> month <*> day
+                where month = dP toMonth <$> (many1 digit) <* seppr 
+                      day   = dP toDay   <$> (many1 digit)
+                      seppr = (char '/')
+                      dP f  = f . read
 
 {- Elementary parser combinataors. -}
 
