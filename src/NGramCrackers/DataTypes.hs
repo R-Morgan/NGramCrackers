df0220fa0d7d6cbbdaf34cb2e0d3802887b5c8ed
R-Morgan <rmorgan@trystero.is>
2015-03-23 14:28:21 -0700

    Move metadata types to MetadataParsers module

--- src/NGramCrackers/DataTypes.hs
+++ src/NGramCrackers/DataTypes.hs
@@ -20,41 +20,4 @@ data Paragraph = Paragraph [Sentence] deriving (Show, Read, Eq)
 data Sentence = Sentence [Word] deriving (Show, Read, Eq)
 type Word = T.Text
 
-data MetaTag = MetaTag { tag      :: T.Text
-                       , contents :: T.Text
-                       } deriving (Show, Read)
 
-data Date = Date Month Day Year
-              deriving (Show, Read, Eq)
-
-data SDate = SDate Month Day 
-              deriving (Show, Read, Eq)
-
-data Month = Jan | Feb | March | April | May | June | July | Aug |
-             Sept | Oct | Nov | Dec deriving (Show, Read, Eq, Enum)
-
-data Day = Day Int deriving (Show, Read, Eq)
-
-data Year = Year Int deriving (Show, Read, Eq)
-
-
-data Tag = SDF Int | Reference T.Text | Entry Date | Pages PageRange |
-           Title T.Text | Publication T.Text Year | FstEdition T.Text Year |
-           Notes T.Text | Authors [T.Text] | Gender [T.Text] |
-           Race [T.Text] | SuperField Int | Subject T.Text |
-           IndividualSub T.Text | Time Year | Region T.Text | Length Int
-           deriving (Show, Read)
-
-{- Date related data declarations -}
-
--- Page Range
-data PageRange =  PageRange PageBound PageBound  deriving (Show, Read, Eq)
-data PageBound = Start Int | End Int deriving (Show, Read, Eq)
-
--- Difficult y Level
-data Level = LitFic | PopFic | Tech | Lay | PopNonFic
-
-data TargetAudience = Adult | Children
-data TargetGender = Female | Male | Trans | NonBinary | Genderfluid | GenderQ
-
-data Biography = Yes | No | Autobiography
