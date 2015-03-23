02f2034ac5b2cd93787aaaa06ca1cd3c82dcd905
R-Morgan <rmorgan@trystero.is>
2015-03-22 21:58:52 -0700

    Move Metadata type declarations to MetadataParsing module
      -- Because of what may be export declaration problems, I moved
         the metadata types to the metadata parsing module to do a
         quick dirty patch to get it working again.  This is not an
         end state, but a temporary work around.
    
      -- Other types were added to the export declaration. These may
         not export correctly yet.

--- src/NGramCrackers/DataTypes.hs
+++ src/NGramCrackers/DataTypes.hs
@@ -8,33 +8,21 @@ module NGramCrackers.DataTypes
 , SDate
 , Month
 , Day
+, Year
+, PageRange
+, PageBound
+, Level
 ) where
 
 import qualified Data.Text as T
 
 data Paragraph = Paragraph [Sentence] deriving (Show, Read, Eq)
-data Sentence = Sentence [Word] deriving (Show, Read, Eq) 
-type Word = T.Text 
+data Sentence = Sentence [Word] deriving (Show, Read, Eq)
+type Word = T.Text
 
-data MetaTag = MetaTag { tag      :: T.Text,
-                         contents :: T.Text
-                       } deriving (Show)
-
-data Tag = SDF Int | Reference T.Text | Entry Date | Pages PageRange |
-           Title T.Text | Publication T.Text Year | FstEdition T. Text Year |
-           Notes Maybe T.Text | Authors [T.Text] | Gender [T.Text] |
-           Race [T.Text] | SuperField Int | Subject T.Text |  
-           IndividualSub T.Text | Time Year | Region T.Text | Length Int |
-           deriving (Show, Read)
-
-{- mkTag :: T.Text -> Tag
-mkTag txt int = case txt of
-                  "SDF" -> SDF int
-                  "REF" -> Reference
--}
-
-
-{- Date related data declarations -}
+data MetaTag = MetaTag { tag      :: T.Text
+                       , contents :: T.Text
+                       } deriving (Show, Read)
 
 data Date = Date Month Day Year
               deriving (Show, Read, Eq)
@@ -42,12 +30,22 @@ data Date = Date Month Day Year
 data SDate = SDate Month Day 
               deriving (Show, Read, Eq)
 
-data Month = Jan | Feb | March | April | May | June | July | Aug | 
+data Month = Jan | Feb | March | April | May | June | July | Aug |
              Sept | Oct | Nov | Dec deriving (Show, Read, Eq, Enum)
 
 data Day = Day Int deriving (Show, Read, Eq)
 
 data Year = Year Int deriving (Show, Read, Eq)
+
+
+data Tag = SDF Int | Reference T.Text | Entry Date | Pages PageRange |
+           Title T.Text | Publication T.Text Year | FstEdition T.Text Year |
+           Notes T.Text | Authors [T.Text] | Gender [T.Text] |
+           Race [T.Text] | SuperField Int | Subject T.Text |
+           IndividualSub T.Text | Time Year | Region T.Text | Length Int
+           deriving (Show, Read)
+
+{- Date related data declarations -}
 
 -- Page Range
 data PageRange =  PageRange PageBound PageBound  deriving (Show, Read, Eq)
@@ -56,10 +54,7 @@ data PageBound = Start Int | End Int deriving (Show, Read, Eq)
 -- Difficult y Level
 data Level = LitFic | PopFic | Tech | Lay | PopNonFic
 
-data TargetAudience = Adult | Children 
+data TargetAudience = Adult | Children
 data TargetGender = Female | Male | Trans | NonBinary | Genderfluid | GenderQ
 
 data Biography = Yes | No | Autobiography
-
-
-
