a4fcb72bb8a6811c878fc921f16dd908dbedf122
R-Morgan <rmorgan@trystero.is>
2015-04-01 10:42:25 -0700

    Update compile and execution scripts
     - The compile and parseCrackers scripts were updated to use
       functions. After sourcing these files, it is possible to
       call different functions to either compile the programme or
       run the programme with or without profiling information.

--- src/compile.sh
+++ src/compile.sh
@@ -1,3 +1,5 @@
-#!/bin/bash
+#! /bin/bash
 
+function ngc-compile() { 
 ghc -Wall -O2 -o GramCrackers Main.hs -prof -v
+}
--- src/parseCrackers.sh
+++ src/parseCrackers.sh
@@ -1,1 +1,9 @@
+#! /bin/bash
+
+function ngc-profile () { 
 ./GramCrackers -i ../doc/examples/multiParaStory.txt -o processed.txt -b +RTS -s -RTS -p
+}
+
+function ngc-pure () { 
+./GramCrackers -i ../doc/examples/multiParaStory.txt -o processed.txt -b
+}
