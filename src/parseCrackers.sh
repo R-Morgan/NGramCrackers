cd2e34f81fa683850c9dab8f08a07946d7b9e8af
R-Morgan <rmorgan@trystero.is>
2015-04-10 19:09:57 -0700

    Update parseCrackers.sh
     - The functions defined here did not use the right binary, so that
       was changed.
    
     - Functions were updated so they would have the appropriate mode
       specified in the ghc call.

--- src/parseCrackers.sh
+++ src/parseCrackers.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-function ngc-profile () { 
-./GramCrackers -i ../doc/examples/multiParaStory.txt -o processed.txt -b +RTS -s -RTS -p
+function ngc-extract-profile () { 
+./ngc extract -i ../doc/examples/multiParaStory.txt -o processed.txt -b +RTS -s -RTS -p
 }
 
-function ngc-pure () { 
-./GramCrackers -i ../doc/examples/multiParaStory.txt -o processed.txt -b
+function ngc-extract-pure () { 
+./ngc extract -i ../doc/examples/multiParaStory.txt -o processed.txt -b
 }
