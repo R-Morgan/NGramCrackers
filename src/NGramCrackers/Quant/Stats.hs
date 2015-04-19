672be2681d1b8e01ef48262f6b8d5c60cc5d0744
R-Morgan <rmorgan@trystero.is>
2015-04-18 22:11:18 -0700

    Add bigramMI function
     - This function took a lot of work. Quite a bunch of code is kinda
       hidden. A map and set of each words and bigrams was needed.
       Although this may be inefficient for calculating a single MI score
       but the maps and sets seem to be more efficient for look ups.
    
     - Adjusted the parameterisation of the pMI and pMI' functions. The
       pMI function handles the MaybeS that are generated in the lookup
       process.
    
     - For some reason I was having some problems with getting things
       to Int, so I changed the total count's type to Double. Not an
       ideal solution exactly.
    
     - Also not sure why (/total) <$> ((fromIntegral <$> snd' bgWc) is
       not equal to ((/total) . fromIntegral) <$> snd' bgWC.
    
     - The best thing, though, is that bigraMI is actually returning
       results.

--- src/NGramCrackers/Quant/Stats.hs
+++ src/NGramCrackers/Quant/Stats.hs
@@ -17,6 +17,7 @@ import qualified Data.Set as S
 
 import NGramCrackers.Ops.Text
 import NGramCrackers.Utilities.List
+import NGramCrackers.Utilities.Tuple
 import NGramCrackers.Parsers.Paragraph
 import NGramCrackers.Quant.Dispersion
 import NGramCrackers.Quant.Counts
@@ -36,20 +37,55 @@ ttrSet' tokens = (typesTot, tokenTot, ratio)
                          -- Could this be done more efficiently w/Vector?
                          ratio    = typesTot / tokenTot
 
-{- bigramMIProfile :: [[[T.Text]]] -> M.Map T.Text Double
+-- Here I need:
+--    - a Set of Words
+--    - a Set of Bigrams
+--    - a Map of bigram frequencies
+--    - a Map of word frequencies
+bigramMI:: T.Text -> [[[T.Text]]] -> (T.Text, Maybe Double)
+bigramMI bg doc = (bg, mutInf) where
+                           concattedDoc = (concatMap concat) doc
+                           mutInf = pMI bgFreq pW1 pW2 total
+                           wset   = wordSet concattedDoc
+                           bset   = bigramSetDoc doc
+                           wmap   = wcMap doc
+                           bmap   = bigramMap doc
+                           total  = (fromIntegral . length) concattedDoc
+                           bgWC   = bigramWordsLookup bg wmap
+                           pW1    = (/total) <$> ((fromIntegral) <$> snd' bgWC)
+                           pW2    = (/total) <$> ((fromIntegral) <$> thrd bgWC)
+                           bgFreq = M.lookup bg bmap
+                           --fmDiv  = fmap (( /total) . fromIntegral)
+
+{-
+bigramMIProfile :: [[[T.Text]]] -> M.Map T.Text Double
 bigramMIProfile doc = pMI bgFreq pW1 pW2 total where
-                        bgFreq =
-                        bmap   = bigramMap doc
+                        wset   = (wordSet . concatMap concat) doc
+                        bset   = bigramSetDoc doc
                         wmap   = wcMap doc
-                        wset   = (map wordSet . concatMap concat) doc
-                        bigram = elemAt 0 wset
+                        bmap   = bigramMap doc
+                        total  = (length . concatMap concat) doc
+                        bgFreq = M.lookup bigram bmap
+                        pW1    = w1c / total
+                        pW2    = w2c / total 
+                        w1c    = (snd . bigramWordsLookup) bmap
+                        w2c    = (thrd . bigramWordsLookup) bmap
+                        bigram = S.elemAt 0 bset
+                        bigramList = DL.concatMap (extractor . T.unwords) . DL.concat doc
 -}
 
-{-| Pointwise mutual information score calculation based on Church and Hanks -}
-pMI :: Int -> Double -> Double -> Int -> Double
-pMI bgFreq pW1 pW2 total = log $ count  / (pW1 * pW2 * totCount) 
-                             where count = fromIntegral bgFreq
-                                   totCount = fromIntegral total
+pMI :: Maybe Int -> Maybe Double -> Maybe Double -> Double -> Maybe Double
+pMI Nothing _ _ _ = Nothing
+pMI _ Nothing _ _ = Nothing
+pMI _ _ Nothing _ = Nothing
+pMI (Just count) (Just pW1) (Just pW2) total = Just $ pMI' count pW1 pW2 total
+
+{-| Pointwise mutual information score calculation for bigrams
+    based on Church and Hanks -}
+pMI' :: Int -> Double -> Double -> Double -> Double
+pMI' bgFreq pW1 pW2 total = log $ count / (pW1 * pW2 * total)
+                              where count    = fromIntegral bgFreq
+
 {-| Takes a parsed paragraph and gets the mean length of the
     sentences in it. -}
 meanSentLength :: [[T.Text]] -> Double
