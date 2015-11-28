a12d2715661b6b7ae93bd8c70dfaecebc9eb945a
R-Morgan <rmorgan@trystero.is>
2015-11-28 11:18:47 -0800

    Refactor module to use NG type
     - Several functions, such as bigramMap or ngramMap, used to
       need to use T.unwords to recompose the ngrams into longer
       T.Text chunks. Since we get directly to lists of NGs, T.unwords
       is no longer necessary.
    
     - Much of the refactoring involves just changing the type signature.
    
     - bigramWordsLookup needed to have some elements unwrapped
       from the Maybe context (e.g., a, b, and wrds). This may not
       be the best way to do this, but it does work for now. Writing
       a function that uses pattern matching to deal with the Maybe
       may also work. It would be nice to get around having to do
       re-injection into the NG context.
    
     - Restructured import statement for neatness and to import
       the appropriate data types.
    
     - Refactored test to be  a DocCol.
     DocCol.

--- src/NGramCrackers/Quant/Counts.hs
+++ src/NGramCrackers/Quant/Counts.hs
@@ -19,44 +19,46 @@ import qualified Data.Maybe as M
 import qualified Data.Set   as S
 import qualified Data.Text  as T
 
-import NGramCrackers.Ops.Text
+import NGramCrackers.DataTypes
 import NGramCrackers.Parsers.Paragraph
 import NGramCrackers.Quant.Dispersion
+import NGramCrackers.Ops.Retyped
 import NGramCrackers.Utilities.List
 
-
 {-| Produces Map of bigrams in a document-}
-bigramMap :: [[[T.Text]]] -> M.Map T.Text Int
+bigramMap :: DocCol T.Text -> M.Map (NG T.Text) Int
 bigramMap doc = bigramMap' stream where
                   bigramMap' = foldl countElem M.empty
-                  stream = (concatMap bigrams . map T.unwords . concat) doc
+                  stream = (concatMap bigrams . concat) doc
 
 {-| Looks up counts of a bigrams constituent words in a Map. Useful for the
     calculation of pMI -}
-bigramWordsLookup :: T.Text -> M.Map T.Text Int -> (T.Text, Maybe Int, Maybe Int)
+bigramWordsLookup :: NG T.Text -> M.Map (NG T.Text) Int -> (NG T.Text, Maybe Int, Maybe Int)
 -- TODO: Possibly awkward to use tuple format. Possibly not. Evaluate.
 -- TODO: Generalise this function to ngrams
 bigramWordsLookup bg m = (bg, aC, bC) where
-                    wrds = T.words bg
+                    wrds = T.words $ M.fromJust $ ngram bg
                     aC = M.lookup a m
                     bC = M.lookup b m
-                    a    = head wrds
-                    b    = last wrds
+                    a  = ngInject $ head wrds
+                    b  = ngInject $ last wrds
+                    -- the need for injections here suggest the possibility
+                    -- of implementing a monad for this type.
 
 {-| Looks up bigram's count in a Map. Useful for the calculation of pMI -}
-bigramLookup :: T.Text -> M.Map T.Text Int -> (T.Text, Maybe Int)
+bigramLookup :: NG T.Text -> M.Map (NG T.Text) Int -> (NG T.Text, Maybe Int)
 bigramLookup bg m = (bg, count) where
 -- m should be a Map of bigrams and their counts, not just words
                       count = M.lookup bg m
 
 {-| Generalised version of bigramMap -}
-ngramMap :: (T.Text -> [T.Text]) -> [[[T.Text]]] -> M.Map T.Text Int
+ngramMap :: (SentColl T.Text -> SentColl T.Text) -> DocCol T.Text -> M.Map (NG T.Text) Int
 ngramMap f doc = ngramMap' stream where
                    ngramMap' = foldl countElem M.empty
-                   stream = (concatMap f . map T.unwords . concat) doc
+                   stream = (concatMap f . concat) doc
 
 {-| Borrowed from: http://nlpwp.org/book/chap-words.xhtml. -}
-wcMap :: [[[T.Text]]] -> M.Map T.Text Int
+wcMap :: DocCol T.Text -> M.Map (NG T.Text) Int
 wcMap doc = wcMap' stream where
               wcMap' = foldl countElem M.empty
               stream = concatMap concat doc
@@ -68,21 +70,22 @@ countElem m e = case M.lookup e m of
                   Nothing -> M.insert e 1 m
 
 {- Makes a set from a list of words-}
-wordSet :: [T.Text] -> S.Set T.Text
+wordSet :: [NG T.Text] -> S.Set (NG T.Text)
+-- Haven't used type synonym because it's not necessarily a Sentence collection
 wordSet = S.fromList
 
 {- Makes a set from a docBody -}
-wordSetDoc :: [[[T.Text]]] -> S.Set T.Text
+wordSetDoc :: DocCol T.Text -> S.Set (NG T.Text)
 wordSetDoc = S.fromList . concatMap concat
 
-bigramSetDoc :: [[[T.Text]]] -> S.Set T.Text
-bigramSetDoc = S.fromList . concatMap bigrams . map T.unwords . concat
+bigramSetDoc :: DocCol T.Text -> S.Set (NG T.Text)
+bigramSetDoc = S.fromList . concatMap bigrams . concat
 
-countWordSetElem :: S.Set T.Text -> [[[T.Text]]] -> [(T.Text, Int)]
+countWordSetElem :: S.Set (NG T.Text) -> DocCol T.Text -> [(NG T.Text, Int)]
 countWordSetElem lexSet doc = countWordSetElem' lexSet concattedDoc where
                                 concattedDoc = concatMap concat doc
                             
-countWordSetElem' :: S.Set T.Text -> [T.Text] -> [(T.Text, Int)]
+countWordSetElem' :: S.Set (NG T.Text) -> [NG T.Text] -> [(NG T.Text, Int)]
 countWordSetElem' lexSet concattedDoc | S.null lexSet       = []
                                       | L.null concattedDoc = []
                                       | otherwise = (word, count) : 
@@ -92,21 +95,22 @@ countWordSetElem' lexSet concattedDoc | S.null lexSet       = []
                                           newSet = S.deleteMin lexSet
                                           newDoc = filter (/= word) concattedDoc
 
-test :: [[[T.Text]]]
+test :: DocCol T.Text
 test = [sents, sents', sents''] where
-         sents = map T.words  [ "The quick brown fox jumped over the lazy dog"
+         sents  = map (map ngInject) $ map T.words [ "The quick brown fox jumped over the lazy dog"
                               , "Colourless green ideas sleep furiously"
                               , "Strange watching a VCA film not on purpose"
                               , "Heather Graham is bossy in this movie"
                               , "Each person has ridiculous boundary issues"
                               ]
-         sents' = map T.words [ "The quick brown fox jumped over the lazy dog"
+         sents' =map (map ngInject) $ map T.words [ "The quick brown fox jumped over the lazy dog"
                               , "They had very bad behavioural examples as children"
                               , "I can't stand commercials that are too loud"
                               , "There should be laws against that sort of thing"
                               ]
 
-         sents'' = map T.words [ "The quick brown fox jumped over the lazy dog"
+         sents'' = map (map ngInject) $ map T.words 
+                                [ "The quick brown fox jumped over the lazy dog"
                                 , "This film is very unrealistic"
                                 , "Everyone radiates foolishness"
                                 , "Corinne is horrifyingly mean to her children for no apparent reason"
