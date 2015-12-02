03e7007c48c24021b8e52dd0105d66504679b0f5
R-Morgan <rmorgan@trystero.is>
2015-12-01 19:38:47 -0800

    Add comments

--- src/NGramCrackers/Parsers/Paragraph.hs
+++ src/NGramCrackers/Parsers/Paragraph.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module NGramCrackers.Parsers.Paragraph
-( 
+(
   parseSent
 , parseParagraph
 , parseMultiPara
@@ -73,6 +73,7 @@ number = T.pack <$> many1 digit
 -- Separation parsers, used to parse and discard punctuation
 --------------------------------------------------------------------------------
 
+{-| For parsing common within sentence separators.-}
 seppr :: PT.Parser ()
 -- Since the results of this parser are just thrown away, we need the `void`
 -- function from Data.Functor
@@ -83,7 +84,10 @@ seppr =  void sepprs <|> void newLn
                          <|> (char ':' *> space')
                  newLn  =    many1 (char '\n')
                  space' = char ' '
-
+{-| Intended to parse separators that demarcate the ends of sentences, when
+ -  followed by spaces. Unsure why I was not able to get '. ' and '.' to parse
+ -  as end of sentence markers. Possible solutioon may be to make separate
+ -  parsers and conjoin them in a new one a la sentParts. -}
 eos :: PT.Parser ()
 eos = void sepprs -- <|> void sngls
         where sepprs =    (char '.' <* space')
@@ -94,7 +98,9 @@ eos = void sepprs -- <|> void sngls
                       <|> (char '?')
 -}
               space' = many (char ' ')
-
+{-| Intended to parse the '<para>' sequence so common in the SGML documents,
+    when followed by a a space or newline. Parser may be useful guide for
+    handling the metadata tags. -}
 eop :: PT.Parser ()
 eop = void $ 
   char '<' >> many1 letter >> char '>' <* (void space' <|> void newLn)
